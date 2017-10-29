1e382174f728eb41ce70a0a2ab12214023bbc46e
dimaskin <dimaskin-nv@yandex.ru>
2017-10-29 19:09:34 +0300

    add railway_station_controller && methods

---
+++ app/controllers/railway_station_controller.rb
@@ -0,0 +1,55 @@
+class RailwayStationController < ApplicationController
+  before_action :set_railway_station, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @railway_stations = RailwayStation.all
+  end
+
+  def show
+  end
+
+  def new
+    @railway_station = RailwayStation.new
+  end
+
+  def edit
+  end
+
+  def create
+    @railway_station = RailwayStation.new(railway_station_params)
+
+    respond_to do |format|
+      if @railway_station.save
+        format.html { redirect_to @railway_station, notice: 'Railway station was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @railway_station.update(railway_station_params)
+        format.html { redirect_to @railway_station, notice: 'Railway station was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @railway_station.destroy
+    respond_to do |format|
+      format.html { redirect_to railway_stations_url, notice: 'Railway station was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_railway_station
+      @railway_station = RailwayStation.find(params[:id])
+    end
+
+    def railway_station_params
+      params.fetch(:railway_station, {})
+    end	
+end
---
+++ app/helpers/railway_station_helper.rb
@@ -0,0 +1,2 @@
+module RailwayStationHelper
+end
