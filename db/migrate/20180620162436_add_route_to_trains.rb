8cf4041572c75bf4c784b7f798c49676c03b1334
dimaskin <dimaskin-nv@yandex.ru>
2018-06-21 16:01:32 +0300

    fix errors

--- db/migrate/20180620162436_add_route_to_trains.rb
+++ db/migrate/20180620162436_add_route_to_trains.rb
@@ -1,7 +1,3 @@
-class AddRouteToTrain < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
 class AddRouteToTrains < ActiveRecord::Migration[5.0]
   def change
     add_belongs_to :trains, :route
--- db/migrate/20180621092055_create_carriages.rb
+++ db/migrate/20180621092055_create_carriages.rb
@@ -1,6 +1,9 @@
 class CreateCarriages < ActiveRecord::Migration[5.1]
   def change
     create_table :carriages do |t|
+      t.string     :type_carriage
+      t.integer    :top_seats
+      t.integer    :bottom_seats    
 
       t.timestamps
     end
