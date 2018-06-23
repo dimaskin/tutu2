d18fa5b2ff8d6f4ec733f74fab539f18e7832299
dd <dd@mb-s.ru>
2018-06-24 00:22:42 +0300

    add station position migration && seed data

---
+++ db/migrate/20180623200735_add_station_position_to_railway_stations_routes.rb
@@ -0,0 +1,6 @@
+class AddStationPositionToRailwayStationsRoutes < ActiveRecord::Migration[5.1]
+    def change
+      add_column :railway_stations_routes, :station_position, :integer
+    end
+  end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#5.times do |i|
-#    Product.create(name: "Product ##{i}", description: "A product.")
-#end
 msk = RailwayStation.create(name: "Москва")
 spb = RailwayStation.create(name: "Санкт-Петербург")
 nv  = RailwayStation.create(name: "Нижневартовск")
 srg = RailwayStation.create(name: "Сургут")
 
-r1 = Route.create(name: "Москва-Сургут-Нижневартовск")
+r1 = Route.create(name: "Москва - Сургут - Нижневартовск")
 r1.railway_stations << msk << srg << nv
+#set staiton_position manual
+RailwayStationsRoute.where(route: r1).where(railway_station: msk).update(station_position: 1)
+RailwayStationsRoute.where(route: r1).where(railway_station: srg).update(station_position: 2)
+RailwayStationsRoute.where(route: r1).where(railway_station: nv).update(station_position: 3)
+
+r2 = Route.create(name: "Москва - Санкт-Петербург")
+r2.railway_stations << msk << spb
+#set staiton_position manual
+RailwayStationsRoute.where(route: r2).where(railway_station: msk).update(station_position: 1)
+RailwayStationsRoute.where(route: r2).where(railway_station: spb).update(station_position: 2)
 
 t1 = Train.new
 t1.number = 1234
 t1.current_station = msk
 t1.route = r1
+t1.save
+
+t2 = Train.new
+t1.number = 9876
+t1.current_station = msk
+t1.route = r2
 t1.save
