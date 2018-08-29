91c91523fc4b7016efcff68c5827cce6171e279f
dimaskin <dimaskin-nv@yandex.ru>
2018-08-29 18:11:57 +0300

    add search form

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   
   root "welcome#index"
 
-  #root "railway_stations#index"
   resources :trains do
     resources :carriages, shallow: true
   end
   resources :railway_stations do
-    patch :update_position, on: :member
+    patch :update_position, :update_arrive_time, :update_depart_time, on: :member
   end
   resources :routes
-  
+  resource  :searches, only: [:new, :show, :edit]
 end
