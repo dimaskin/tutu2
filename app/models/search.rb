f3896d054465fd89c3a658d12006de569a720c1e
dimaskin <dimaskin-nv@yandex.ru>
2018-08-31 17:42:10 +0300

    fix model search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,12 @@
 class Search < ApplicationRecord
 
   def search_routes
-    #Route.joins(:railway_stations).where(railway_stations: {id:[start_rws,finish_rws]})
+    Route.joins(:railway_stations_route).where(railway_stations: {id:[start_rws,finish_rws]})
   end
 
 end
+
+#Person
+#  .select('people.id, people.name, companies.name')
+#  .joins(:company)
+#  .find_by('people.name' => 'John') # this should be the last
